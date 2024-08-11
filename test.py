304f4b6e792f2e3dd98dc7081e89cc173f25daa2
Reah <rsardanas@up.edu.ph>
2024-08-11 15:25:22 +0800

    init

--- test.py
+++
@@ -1,8 +0,0 @@
-import fullbody_movement_tracker.movenet as movenet
-
-modelGetter = movenet.Detector(isTflite=True)
-
-test = modelGetter.loadModel("movenet_lightning_f16")
-test = modelGetter.doImageInference('./instructor-jpeg/0001.jpg')
-
-print(test)
